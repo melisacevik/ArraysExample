16383458ad5b0f9903c93c34c54146734a3d71eb
melisacevik <celissamevik@gmail.com>
2022-12-29 16:24:24 +0300

    CSharpArraysExmp

---
+++ CSharpArraysExmp/Program.cs
@@ -0,0 +1,47 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+
+    {
+
+
+        /*Bir dize 19 elemandan oluşuyor ve bu elemanların herhangi birine
+          bu şekilde ulaşabiliriz.
+        int [] LuckyNumbers= new int [19];
+        LuckyNumbers[0] = 56;
+        LuckyNumbers[1] = 54;
+        LuckyNumbers[2] = 78;
+        LuckyNumbers[3] = 89; 
+        LuckyNumbers[4] = 45;
+        LuckyNumbers[5] = 53;
+        LuckyNumbers[6] = 27;
+        LuckyNumbers[7] = 89;
+        LuckyNumbers[8] = 90;
+        LuckyNumbers[9] = 41;
+        LuckyNumbers[10] = 52;
+        LuckyNumbers[11] = 14;
+        LuckyNumbers[12] = 70;
+        LuckyNumbers[13] = 23;
+        LuckyNumbers[14] = 55;
+        LuckyNumbers[15] = 78;
+        LuckyNumbers[16] = 89;
+        LuckyNumbers[17] = 11;
+        LuckyNumbers[18] = 14;
+        
+
+        Console.WriteLine("Lucky Number: " + LuckyNumbers[13]);
+        Console.ReadLine();
+
+        
+
+
+        */
+
+
+
+
+
+
+
+    }
+}
